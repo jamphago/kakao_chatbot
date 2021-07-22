00f1d8c008fe1f5e6994317a217b8a38f95a8618
jamphago <75367863+jamphago@users.noreply.github.com>
2021-07-22 10:46:40 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,14 +11,6 @@ function response(room, msg, sender, isGroupChat, replier) {
     msg = msg.trim();
 	if(msg == "/메뉴") {
 			var pick = menu[Math.floor(Math.random()*menu.length)];
-			/*var pick2 = member[Math.floor(Math.random()*9)];
-			var now = new Date();
-			var then = new Date("December 18,2020 09:00:00");
-			var gap = then.getTime()-now.getTime();
-			gapas = Math.floor((gap%(1000*60*60))/(1000*60)); //분
-			gapa = Math.floor((gap%(1000*60*60*24))/(1000*60*60)); //시
-			gap = Math.floor(gap/(1000*60*60*24));//일*/
-		   
 			replier.reply(sender + "님 오늘의 추천 메뉴는 "+pick+"입니다!\n\n"+"음식을 추가하고 싶은 경우 '/추가 음식명' 명령어를 입력해주세요!");
 			}
        
